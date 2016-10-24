1d272ad9a52dfb5d4a83f8180b3fa4fc5c1c9b25
蒋治国 <727918130@qq.com>
2016-10-24 19:00:23 +0800

    gradle

---
+++ app/src/main/java/com/bjxrgz/startup/dao/RedDao.java
@@ -0,0 +1,10 @@
+package com.bjxrgz.startup.dao;
+
+/**
+ * Created by JiangZhiGuo on 2016-10-24.
+ * describe
+ */
+
+public class RedDao {
+
+}
---
+++ app/src/main/java/com/bjxrgz/startup/domain/Red.java
@@ -0,0 +1,10 @@
+package com.bjxrgz.startup.domain;
+
+/**
+ * Created by JiangZhiGuo on 2016-10-24.
+ * describe
+ */
+
+public class Red {
+
+}
